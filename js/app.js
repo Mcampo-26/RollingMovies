104d82f39c3969c6681e03009a4ea35b1a4b103b
Mauricio Campo <mcampo26@gmail.com>
2023-05-07 12:27:09 -0300

    correcion responsiove

---
+++ js/app.js
@@ -0,0 +1,29 @@
+function renderizarInfo(){
+    let contenedorSeriesPeliculas = JSON.parse(localStorage.getItem("SeriesPeliculasKey")) || [];   
+    console.log(contenedorSeriesPeliculas)
+
+    let bloque = document.getElementById('bloqueDeSeriesPeliculas');
+    bloque.innerHTML = '';
+    
+    for(let elementosSeriesPeliculas of contenedorSeriesPeliculas) {
+
+         let columna = `         
+            <div class="pelis-col  pelis-col col-sm-3 col-md-3 col-lg-2">
+            <div class="card card-crud">                
+                    <div>
+                        <img src="${elementosSeriesPeliculas.imagen}" class="card-img-top img-card-crud " alt="${elementosSeriesPeliculas.nombre}" >
+                    </div>              
+                <div class="card-body  bg-black">
+                    <h5 class="card-title text-light">${elementosSeriesPeliculas.nombre}</h5>
+                    <p class="card-text text-light">${elementosSeriesPeliculas.descripcion}</p>
+                    <a href="error404.html" target="_blank" class="btn btn-primary bg bg-dark">go to</a>
+                </div>
+                </div>
+            </div>
+            
+            `;
+            bloque.innerHTML += columna;
+    }  
+}
+
+renderizarInfo();
---
+++ js/log_in.js
@@ -0,0 +1,61 @@
+document.getElementById("login-form").reset();
+
+function redirigirUsuario() {
+    window.location.href = 'Home.html';
+}
+function redirigiAdmin() {
+    window.location.href = 'admin.html';
+}
+
+function logearUsuario(){
+    event.preventDefault();
+
+    let regemail = document.getElementById("reg-email").value;
+    let regcontrasena = document.getElementById("reg-contrasena").value;
+
+    let newEmail = document.getElementById("reg-email").value;
+    let newPassword = document.getElementById("reg-contrasena").value;
+
+    console.log(regemail,regcontrasena) /* borrar */
+
+    let datosNewUsers = JSON.parse(localStorage.getItem("datosNewUserssKey")) || [];
+
+    console.log(datosNewUsers); /* borrar */
+
+    
+    
+    if (newEmail === "admin" && newPassword === "admin") {
+         Swal.fire(
+            'Registro de Administrador Exitoso',
+            'Ingresando al panel de administrador',
+            'success'
+        );
+        setTimeout(function() {
+            redirigiAdmin();
+        }, 3000);
+        return;
+    }
+    
+
+     for (let i = 0; i < datosNewUsers.length; i++) {
+        if (datosNewUsers[i].newEmail === newEmail && datosNewUsers[i].newPassword === newPassword) {
+            Swal.fire(
+                'Registro Exitoso',
+                'Ingresando a tu cuenta de Netflix',
+                'success'
+            );
+            setTimeout(function() {
+                redirigirUsuario();
+            }, 3000);
+            return;
+        }else{
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Ingresa un email/contraseña válido. '
+              })
+        }
+    }
+    
+    document.getElementById("login-form").reset();
+}
