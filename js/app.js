efbb3db35ec66859c3ef4dd431c14c923b6709a8
Mauricio Campo <mcampo26@gmail.com>
2023-05-16 16:31:03 -0300

    corrijo enlace de pelicula crud

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ function renderizarInfo(){
                 <div class="card-body  bg-black">
                     <h5 class="card-title text-light">${elementosSeriesPeliculas.nombre}</h5>
                     <p class="card-text text-light">${elementosSeriesPeliculas.descripcion}</p>
-                    <a href="error404.html" target="_blank" class="btn btn-primary bg bg-dark">go to</a>
+                    <a href="error404.html" target="_blank" class="btn btn-primary bg bg-dark">ver pelicula</a>
                 </div>
                 </div>
             </div>
